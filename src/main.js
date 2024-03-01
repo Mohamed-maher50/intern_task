47908f94690eadd05b59dc7668bcec4302a10a91
mohamed maher abdelaziz <mohamedmaher.mm330@gmail.com>
2024-03-01 14:03:41 +0200

    apply hove in buttons and links

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const congratulations_template = `
 </p>
 <p class="text-3xl py-3">Call before enrollment closes!</p>
 <a href="tel:+18882277051"
-    class=" bg-blue-500 block w-full text-[25px] rounded-md font-bold text-white my-3 p-4">Call
+    class=" bg-blue-500 hover:bg-blue-300 block w-full text-[25px] rounded-md font-bold text-white my-3 p-4">Call
     Now (888) 227-7051</a>
 <p class="text-2xl py-2">ACT FAST! Due to high call demand, your hotline will remain available for
     the
