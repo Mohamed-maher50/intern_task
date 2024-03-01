4e1052d328c917ed84bf5dc02f12d93968c84bd5
mohamed maher abdelaziz <mohamedmaher.mm330@gmail.com>
2024-03-01 23:08:31 +0200

    restyle pure css

--- README.md
+++
@@ -1,2 +0,0 @@
-# Preview the live landing page
-## <ins>Demo</ins> : https://mohamed-maher50.github.io/intern_task/src/index.html
--- main.js
+++ main.js
@@ -14,44 +14,45 @@ const questionContent = document.getElementById("question-content");
 const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
 const congratulations_template = `
-<div class="lg:py-5 lg:px-16 text-center">
-<h2 class=" border-t-8 pt-10 text-4xl font-bold border-[#00007c]">CONGRATULATIONS,
+<div class=" text-center">
+<h2 class=" border-t">CONGRATULATIONS,
     You're
     Eligible!</h2>
-<p class="text-3xl   py-4 ">
+<p >
     <b>Last Step:</b>You must
     <span class="underline">Call Now</span>
-    to receive your <span class="text-red-400">Free Credits</span>
+    to receive your <span style="color:red;">Free Credits</span>
     with Medical, Dental &amp; Vision coverage.
 
 </p>
-<p class="text-3xl py-3">Call before enrollment closes!</p>
+<p >Call before enrollment closes!</p>
 <a href="tel:+18882277051"
-    class=" bg-blue-500 hover:bg-blue-300 block w-full text-[25px] rounded-md font-bold text-white my-3 p-4">Call
+
+    class="  btn w-full block">Call
     Now (888) 227-7051</a>
-<p class="text-2xl py-2">ACT FAST! Due to high call demand, your hotline will remain available for
+<p >ACT FAST! Due to high call demand, your hotline will remain available for
     the
-    next: <span class="text-red-400">00:00</span> </p>
+    next: <span >00:00</span> </p>
 </div>
 
 `;
-questionContent.innerHTML = `<p class='w-fit mx-auto text-3xl p-3'>${questions[questionIndex].question}</p>`;
+questionContent.innerHTML = `<p class='question text-center'>${questions[questionIndex].question}</p>`;
 yesButton.addEventListener("click", () => {
   if (questionIndex == 0) {
     questions[0].answer = "yes";
-    return (questionContent.innerHTML = `<p class='w-fit mx-auto text-3xl p-3'>${
+    return (questionContent.innerHTML = `<p class='question text-center'>${
       questions[++questionIndex].question
     }</p>`);
   }
   if (questions[questionIndex]) {
     questions[questionIndex].answer = "yes";
     if (questions[++questionIndex])
-      return (questionContent.innerHTML = `<p class='w-fit mx-auto text-3xl p-3'>${questions[questionIndex].question}</p>`);
+      return (questionContent.innerHTML = `<p class='question text-center'>${questions[questionIndex].question}</p>`);
     if (questions[0].answer == "no" && questions[1].answer == "no") {
       return (questionsContainer.innerHTML = congratulations_template);
     } else {
       return (questionsContainer.innerHTML = `
-        <h2 class=" max-md:text-2xl text-5xl pb-20 font-bold text-center">Unfortunately, we can't Qualify you at the moment</h2>
+        <h2 class=" text-center">Unfortunately, we can't Qualify you at the moment</h2>
         `);
     }
   }
@@ -59,20 +60,20 @@ yesButton.addEventListener("click", () => {
 noButton.addEventListener("click", () => {
   if (questionIndex == 0) {
     questions[0].answer = "no";
-    return (questionContent.innerHTML = `<p class='w-fit mx-auto text-3xl p-3'>${
+    return (questionContent.innerHTML = `<p >${
       questions[++questionIndex].question
     }</p>`);
   }
   if (questions[questionIndex]) {
     questions[questionIndex].answer = "no";
     if (questions[++questionIndex])
-      return (questionContent.innerHTML = `<p class='w-fit mx-auto text-3xl p-3'>${questions[questionIndex].question}</p>`);
+      return (questionContent.innerHTML = `<p>${questions[questionIndex].question}</p>`);
 
     if (questions[0].answer == "no" && questions[1].answer == "no") {
       return (questionsContainer.innerHTML = congratulations_template);
     } else {
       return (questionsContainer.innerHTML = `
-      <h2 class="text-xl font-bold text-center">Unfortunately, we can't Qualify you at the moment</h2>
+      <h2>Unfortunately, we can't Qualify you at the moment</h2>
       `);
     }
   }
